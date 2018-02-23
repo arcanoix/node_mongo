653c10eb4c252f227fb449a3b4171c5249b30301
thavoo <gustavoh.2312@gmail.com>
2018-02-23 04:40:43 -0400

    create server init node

--- api_rest/index.js
+++ api_rest/index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extend: false } ))
 app.use(bodyParser.json())
 
+app.get('/hola/:name', (req, res) => {
+  res.send({ message: `Hola ${req.params.name}` })
+})
+
 app.listen(port, () => {
   console.log(`Api rest corriendo en http://localhost:${port} `)
 })
